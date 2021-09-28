b5d75834bba88eed5d26d0e42db3652133c5437f
oguzduzgun14 <oguzduzgun1414@gmail.com>
2021-09-28 17:08:44 +0300

    Initial commit

---
+++ src/variablesExampel/Main.java
@@ -0,0 +1,41 @@
+package variablesExampel;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String phoneName="Xiomi Redmi Note 10";
+		double phonePrice=3204.44;
+		double phoneStar=5;
+		boolean phoneIsDiscound=true;
+		int phoneDiscound=4;
+		double phoneScreenSize=6.43;
+		int phoneÝnternalmemory=64;
+		int phoneRamCapactiy=8;
+		double phoneBatteryPower=5000;
+		double phoneFrontCamera=13;
+		double phoneCameraResolution=64+8+2;
+		System.out.println("telefonun markasý :"+phoneName);
+		System.out.println("telefonun fiyatý :"+ phonePrice);
+		System.out.println("telefonun inrimi :"+phoneIsDiscound);
+		System.out.println("telefonun indirim miktaý :"+phoneIsDiscound);
+		System.out.println("ekran boyutu :"+phoneScreenSize);
+		System.out.println("dahili hafýza :"+phoneÝnternalmemory);
+		System.out.println("ram kapasitesi :"+phoneRamCapactiy);
+		System.out.println("batarya gücü :"+phoneBatteryPower);
+		System.out.println("ön kamera :"+phoneFrontCamera);
+		System.out.println("kamera çözünürlüðü :"+phoneCameraResolution);
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
